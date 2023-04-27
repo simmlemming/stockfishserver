d9e020ff582ba2df56149aade23701d878dbf613
Maxim Tkachenko <maxim.tkachenko@tomtom.com>
2023-04-27 20:21:39 +0200

    Stockfish methods can be called.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
 from server import start_server
+import argparse
 
 if __name__ == '__main__':
-    start_server()
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('--stockfish-bin', dest='stockfish_bin', required=True)
+
+    args = parser.parse_args()
+
+    if args.stockfish_bin == "fake":
+        from fake_stockfish import Stockfish
+    else:
+        from stockfish import Stockfish
+
+    start_server(Stockfish(args.stockfish_bin))
--- app/server.py
+++ app/server.py
@@ -4,27 +4,56 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class StockfishRouter(BaseHTTPRequestHandler):
 
-    def do_GET(self):
-        body = {
-            'message': 'Hello World!'
-        }
+    def do_POST(self):
+        if self.path != '/call':
+            self.do_send_text_response(500, f"Invalid path.")
+            return
 
-        self.do_send_json_response(200, body)
+        request_body = self.read_body()
 
-    def do_send_json_response(self, code, body):
+        if not request_body:
+            self.do_send_text_response(500, f"Cannot read request body: {str(request_body)}")
+            return
+
+        if "method" not in request_body:
+            self.do_send_text_response(500, f"'method' not found")
+            return
+
+        sf_method_name = request_body["method"]
+        sf_method = getattr(sf, sf_method_name, None)
+
+        if not sf_method:
+            self.do_send_text_response(500, f"Invalid method: {str(sf_method_name)}")
+            return
+
+        args = request_body.get("args", {})
+        body = sf_method(*args)
+
+        self.do_send_text_response(200, body)
+
+    def read_body(self):
+        if self.headers.get("Content-Type") == "application/json":
+            length = int(self.headers.get("Content-Length"))
+            return json.loads(self.rfile.read(length).decode("utf-8"))
+        return None
+
+    def do_send_text_response(self, code, body):
         self.send_response(code)
         if body:
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(bytes(json.dumps(body), "utf-8"))
+            self.wfile.write(bytes(body, "utf-8"))
         else:
             self.end_headers()
 
-    def do_send_error(self, code, message):
-        self.do_send_json_response(code, message)
+
+sf = None
 
 
-def start_server():
+def start_server(stockfish):
+    global sf
+    sf = stockfish
+
     server = HTTPServer(("0.0.0.0", 8080), StockfishRouter)
     try:
         print(f"Starting on {server.server_address}...")
